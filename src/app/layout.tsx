4aa5552f43595706e51132712685ae5c47430d44
Kasper Klæboe Berg <kasperkberg@gmail.com>
2025-10-08 20:38:34 +0200

    Revert "webview redirect"
    This reverts commit e330e6af1ef86173ced4af0fcc7429d5ffe3f58d.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "./providers";
-import { WebViewRedirect } from "./components/WebViewRedirect";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#c4cdbf]`}
       >
-        <WebViewRedirect />
         <Providers>{children}</Providers>
       </body>
     </html>
